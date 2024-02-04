c9b941eb49c835e839852a706fd7eb9068c17475
NUKUS777 <dawletkenesbaev620@gmail.com>
2024-02-04 19:44:04 +0500

    group routinh

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const SignUp = () => {
+    console.log('where do i render');
+    
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
